960bc5e327aad85511064d88716909b40c8aadf3
Tamar Simon <tamar119113@gmail.com>
2022-09-18 10:34:46 +0300

    Create dto object

--- controllers/calcaulator.controller.js
+++ controllers/calcaulator.controller.js
@@ -1,11 +1,12 @@
 
 var calcaulatorService = require('../services/calculate.service');
+var requestDtoService = require('../dto/requsetDTO.dto');
 
 //this function get 2 paramteres and operator and calculate the result.
 exports.calculate = async (req, res) => {
-    const { number1, number2, operator } = req.body ;
-    let operatorInsance =  calcaulatorService.getOperatorInstance(operator);
-    const result = operatorInsance.calculate(parseInt(number1),parseInt(number2));
+    let requestDto = requestDtoService.requestDto(req.body);
+    let operatorInsance =  calcaulatorService.getOperatorInstance(requestDto.operator);
+    const result = operatorInsance.calculate(requestDto.number1,requestDto.number2);
     console.log(result)
     return res.status(200).send({result:result});
 }
---
+++ dto/requsetDTO.dto.js
@@ -0,0 +1,10 @@
+//Dto data transfer object  protect your API clients from changes made on the server
+//using function base approach
+function requestDto(requset){
+  return { number1, number2, operator } = requset ;
+}
+
+module.exports = {
+  requestDto: requestDto
+}
+
